c753105d2100369a3b9e71642593172cbacfb0a0
dingdong9763 <dingmingyue8888@163.com>
2021-01-11 11:28:56 +0800

    Update Singleton.h

--- cpp-part1/Singleton.h
+++ cpp-part1/Singleton.h
@@ -4,20 +4,20 @@
 #include <iostream>
 using namespace std;
 
-class Singlenton {
+class Singleton {
 public:
-    static Singlenton& getInstance();
+    static Singleton& getInstance();
     void setup() {
         cout << "setuping"<< endl;
     };
 private:
-    Singlenton(){cout<<"create"<<endl;};
-    Singlenton(const Singlenton& rhs);
+    Singleton(){cout<<"create"<<endl;};
+    Singleton(const Singleton& rhs);
 };
 
-Singlenton& Singlenton::getInstance() {
-    // 只有调用的时候才创建这个静态的对象 多次调用也只是返回一个
-    static Singlenton s;
+Singleton& Singleton::getInstance() {
+    // 只有当调用的时候才创建这个静态的对象，多次调用也只是返回一个，如果没有人使用的话这个单例就不存在
+    static Singleton s;
     return s;
 }
 
