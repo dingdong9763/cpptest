7f575529d7bcb4251ab396338665954298bd21a9
dingdong9763 <dingmingyue8888@163.com>
2021-01-15 23:44:24 +0800

    Update pointer-like.cpp

--- cpp-part2/pointer-like.cpp
+++ cpp-part2/pointer-like.cpp
@@ -2,19 +2,20 @@
 using namespace std;
 
 // 智能指针
+// 指针有 * 和 -> 操作符，对于一个class去模拟point，我们必须对其 * 和 -> 操作符进行重载。
 template<class T>
 class my_shared_ptr {
 public:
     T& operator*() const {
-        return *px;
+        return *px; // *用完就没有了
     }
 
     T* operator->() const {
-        return px;
+        return px; //->用完还有
     }
 
-    my_shared_ptr(T* p): px(p) {}
+    my_shared_ptr(T* p): px(p) {} //构造函数，接受天然指针
 private:
-    T* px;
+    T* px; //一定带有一个一般的指针
     long* pn;
 };
